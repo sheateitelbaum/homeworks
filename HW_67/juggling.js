6ac137fab1aa2a7e29ce471a805cc231789f374b
sheateitelbaum <yyteitelbaum@gmail.com>
2017-02-23 13:45:58 -0500

    corrected juggling code

--- HW_67/juggling.js
+++ HW_67/juggling.js
@@ -1,17 +1,22 @@
- const http = require('http');
+const http = require('http');
 const bl = require('bl');
-
-function juggle (i){http.get(process.argv[i], function (response) {
-    response.pipe(bl(function (err, data) {
-        if (err) {
-            console.log(err);
-        }
-        const collection = data.toString();
-        console.log(collection);
-    }))
-
-})
+const collectionArray = [];
+let index;
+function juggle(i, index) {
+    http.get(process.argv[i], function (response) {
+        response.pipe(bl(function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            collectionArray[index] = data.toString();
+            if (index === 2) {
+                collectionArray.forEach(function (collection) {
+                    console.log(collection);
+                });
+            }
+        }));
+    });
 }
- for(i =2;i<5;i++){
-     juggle(i);
- }  
+for (i = 2; i < 5; i++) {
+    juggle(i, i - 2);
+}  
